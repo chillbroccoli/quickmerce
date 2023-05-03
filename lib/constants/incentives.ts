2a35af997955ba23800a2f6797ef618fa6209e7b
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-03 17:07:09 +0200

    add incentives constant

--- lib/constants/incentives.ts
+++ lib/constants/incentives.ts
@@ -9,23 +9,18 @@ export const incentives = [
     name: "Free Shipping",
     description:
       "It's not actually free we just price it into the products. Someone's paying for it, and it's not us.",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce/icons/icon-delivery-light.svg",
     icon: IconTruckDelivery,
   },
   {
     name: "24/7 Customer Support",
     description:
       "Our AI chat widget is powered by a naive series of if/else statements. Guaranteed to irritate.",
-    imageSrc: "https://tailwindui.com/img/ecommerce/icons/icon-chat-light.svg",
     icon: IconMessageCircle2,
   },
   {
     name: "Fast Shopping Cart",
     description:
       "Look how fast that cart is going. What does this mean for the actual experience? I don't know.",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce/icons/icon-fast-checkout-light.svg",
     icon: IconShoppingBag,
   },
 ];
