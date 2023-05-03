e42c882f39c8648bb68736d8de4c8e5c2e844f86
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-03 17:07:30 +0200

    add product_features constant

---
+++ lib/constants/product_features.ts
@@ -0,0 +1,23 @@
+export const features = [
+  {
+    name: "Adventure-ready",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat id illo officia autem ipsam cum aspernatur tempore, earum animi excepturi eaque? Odio rerum architecto totam debitis sit officiis reprehenderit nesciunt.",
+    imageSrc: "/img/product_5.jpeg",
+    imageAlt: "",
+  },
+  {
+    name: "Minimal and clean",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto ipsam dolorum ipsum necessitatibus, sint eveniet magnam. Consectetur reiciendis ipsam repellendus? Quibusdam, quidem rem! Necessitatibus eum sequi eos iusto qui? Nostrum?",
+    imageSrc: "/img/product_6.jpeg",
+    imageAlt: "",
+  },
+  {
+    name: "Organized",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad vel facilis, quas totam accusantium odit tenetur asperiores illum magnam, iusto aperiam cum corrupti libero eos. Aliquid porro dolore vel perspiciatis.",
+    imageSrc: "/img/product_7.jpeg",
+    imageAlt: "",
+  },
+];
