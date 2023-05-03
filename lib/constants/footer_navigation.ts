b5eca419d7a6ce634b8ce248a4939d9bfb149df3
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-03 17:07:02 +0200

    add footer_navigation constant

---
+++ lib/constants/footer_navigation.ts
@@ -0,0 +1,23 @@
+import {
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandTwitter,
+} from "@tabler/icons-vue";
+
+export const navigation = [
+  {
+    name: "Facebook",
+    href: "#",
+    icon: IconBrandFacebook,
+  },
+  {
+    name: "Instagram",
+    href: "#",
+    icon: IconBrandInstagram,
+  },
+  {
+    name: "Twitter",
+    href: "#",
+    icon: IconBrandTwitter,
+  },
+];
