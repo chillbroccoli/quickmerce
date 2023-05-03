4113c1ec69140c064bf9d19ef992f8efa6dfc10c
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-03 14:51:50 +0200

    add constants files

---
+++ lib/constants/product.ts
@@ -0,0 +1,4 @@
+export const BRANDS = ["Nike", "Adidas", "Puma", "Rebook", "Vans"];
+export const MATERIALS = ["Leather", "Canvas", "Synthetic", "Mesh"];
+export const GENDERS = ["Men", "Women", "Unisex"];
+export const STYLES = ["Running", "Basketball", "Skateboarding", "Casual"];
