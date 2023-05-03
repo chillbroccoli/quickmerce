f431e87bb325ccd34d4dfc127827fe0b6cd4cf05
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-03 14:51:29 +0200

    add home page

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="mt-8">
+      <HeroSection />
+    </div>
+    <div class="mt-16">
+      <ProductFeatures />
+    </div>
+    <div class="mt-16">
+      <FeaturedProducts />
+    </div>
+    <div class="mt-16">
+      <Incentives />
+    </div>
+    <div class="py-8 mt-16">
+      <Footer />
+    </div>
+  </div>
+</template>
