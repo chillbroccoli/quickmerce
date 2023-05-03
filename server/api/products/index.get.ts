32d1c75a84fb6f6d5bf43054d4e8f44a749206af
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-03 14:51:16 +0200

    add products api route

---
+++ server/api/products/index.get.ts
@@ -0,0 +1,12 @@
+import PRODUCTS from "@/lib/data/data.json";
+import { Product } from "~/lib/types/product";
+
+export default defineEventHandler((event) => {
+  const query = getQuery(event) as { start?: number; limit?: number };
+
+  const data = query?.limit
+    ? [...PRODUCTS].slice(query?.start ?? 0, query.limit)
+    : PRODUCTS;
+
+  return data as Product[];
+});
