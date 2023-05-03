285d2c6988640ae6c9730e5fec4e51d2b54473c1
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-03 14:50:56 +0200

    init commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: {
+    enabled: true,
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    configPath: "tailwind.config",
+  },
+  app: {
+    head: {
+      title: "Quickmerce",
+    },
+  },
+});
