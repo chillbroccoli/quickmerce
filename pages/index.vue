b69c3957264454cf37a8c3e30e135bb245c62d1d
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-06 15:38:56 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+  <div class="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8">
     <div class="mt-8">
       <HeroSection />
     </div>
@@ -11,9 +11,6 @@
     </div>
     <div class="mt-16">
       <Incentives />
-    </div>
-    <div class="py-8 mt-16">
-      <Footer />
     </div>
   </div>
 </template>
